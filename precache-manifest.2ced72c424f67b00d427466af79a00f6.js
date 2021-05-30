1b158fe964e02c87501a0d11598fb58a7a2b9e8e
bangblock <mixa2229@gmail.com>
2021-05-30 16:02:46 +0300

    Updates

---
+++ precache-manifest.2ced72c424f67b00d427466af79a00f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a901d5ca8012b63aeadd5679d8dc0eb9",
+    "url": "/bash/index.html"
+  },
+  {
+    "revision": "c863587bff9d75c7628a",
+    "url": "/bash/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "6dd8ca1c9c908795533e",
+    "url": "/bash/static/css/main.7a1116c8.chunk.css"
+  },
+  {
+    "revision": "c863587bff9d75c7628a",
+    "url": "/bash/static/js/2.ace09298.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/bash/static/js/2.ace09298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd8ca1c9c908795533e",
+    "url": "/bash/static/js/main.4da41a2e.chunk.js"
+  },
+  {
+    "revision": "1abb95d69285ec3c83a3",
+    "url": "/bash/static/js/runtime-main.02e0ac53.js"
+  }
+]);
